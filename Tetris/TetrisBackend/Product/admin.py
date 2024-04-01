00b8542bf6eccb07a67c2fccf11b852528610fc0
GodJess <asusfutcor@gmail.com>
2024-04-01 17:53:02 +0300

    Models

--- Tetris/TetrisBackend/Product/admin.py
+++ Tetris/TetrisBackend/Product/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from Product.models import ProductModel, CustomUser
+from Product.models import ProductModel, CustomUser, SalesMan, Snikers, cloth
 
 admin.site.register(ProductModel)
 admin.site.register(CustomUser)
+admin.site.register(cloth)
+admin.site.register(SalesMan)
+admin.site.register(Snikers)
 # Register your models here.
--- Tetris/TetrisBackend/Product/models.py
+++ Tetris/TetrisBackend/Product/models.py
@@ -10,6 +10,9 @@ class ProductModel(models.Model):
     img3 = models.ImageField(upload_to='images/')
     dateOfPublic = models.DateField("DateOfPublic")
     discount = models.CharField("Скидка", max_length = 10)
+    salesman = models.CharField("Salesman", max_length = 100, default = '')
+    type = models.CharField('Type', max_length = 30, default = '')
+    linkToSize = models.CharField('Link', max_length = 30, default = "")
     
     def __str__(self):
         return self.name
@@ -17,7 +20,49 @@ class ProductModel(models.Model):
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
+        
 # Create your models here.
+class SalesMan(models.Model):
+    name = models.CharField("Salesman Name", max_length = 30)
+    countofmany = models.CharField("CountOfMoney", max_length = 200)
+    descript = models.TextField("Descript", max_length=300) 
+    mail = models.EmailField("Mail", max_length = 40)
+    grade = models.CharField("greade", max_length = 1)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Salesman'
+        verbose_name_plural = 'Salesmans'
+
+
+class Snikers(models.Model):
+    size35 = models.CharField("count size", max_length = 10)
+    size36 = models.CharField("count size", max_length = 10)
+    size37 = models.CharField("count size", max_length = 10)
+    size38 = models.CharField("count size", max_length = 10)
+    size39 = models.CharField("count size", max_length = 10)
+    size40 = models.CharField("count size", max_length = 10)
+    size41 = models.CharField("count size", max_length = 10)
+    size42 = models.CharField("count size", max_length = 10)
+    size43 = models.CharField("count size", max_length = 10)
+    size44 = models.CharField("count size", max_length = 10)
+    size45 = models.CharField("count size", max_length = 10)
+
+    class Meta:
+        verbose_name = 'SnikersSize'
+        verbose_name_plural = 'SnikersSizes'
+    
+class cloth(models.Model):
+    s = models.CharField("count size", max_length = 10)
+    m = models.CharField("count size", max_length = 10)
+    l = models.CharField("count size", max_length = 10)
+    xl = models.CharField("count size", max_length = 10)
+    xxl = models.CharField("count size", max_length = 10)
+    class Meta:
+        verbose_name = 'ClothSize'
+        verbose_name_plural = 'ClothSizes'
 
 class CustomUser(AbstractUser):
     phone = models.CharField('phone', max_length = 12 )
